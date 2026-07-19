789d006fba203cf617e6c40d35fc885847c3511a
mateopinto0 <pintomateo74@gmail.com>
2026-07-19 20:29:37 -0300

    primer commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBWhahrWoh0jdtL7zRTs_ysX8-6tw2SLjQ",
+  authDomain: "perfumeria-22744.firebaseapp.com",
+  projectId: "perfumeria-22744",
+  storageBucket: "perfumeria-22744.firebasestorage.app",
+  messagingSenderId: "837442544332",
+  appId: "1:837442544332:web:f17ec25751fd797f371869"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
